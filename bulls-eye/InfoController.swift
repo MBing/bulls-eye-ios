a1a2e0519159508cb714a2b7c7870f5604fa9bca
Martin Bing <bing.martin.photography@gmail.com>
2017-12-10 14:04:53 +0100

    Add ctrl for info view with logic

---
+++ bulls-eye/InfoController.swift
@@ -0,0 +1,37 @@
+//
+//  InfoController.swift
+//  bulls-eye
+//
+//  Created by Martin Demiddel on 10.12.17.
+//  Copyright © 2017 Martin Demiddel. All rights reserved.
+//
+
+import UIKit
+
+class InfoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeInfoView() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
